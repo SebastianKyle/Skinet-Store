ca9505592f4a6768b022b3aca77f1757a1cc50b4
SebastianKyle <doantanqn000@gmail.com>
2023-05-19 19:05:34 +0700

    App UI updated with filters, sorting, search box and products details

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IProduct } from './models/product';
-import { IPagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +8,11 @@ import { IPagination } from './models/pagination';
 })
 export class AppComponent implements OnInit {
   title = 'Skinet';
-  products: IProduct[];
 
   constructor(private http: HttpClient) {
 
   }
 
   ngOnInit(): void {
-    this.http.get('https://localhost:7007/api/Products').subscribe((response: IPagination) => {
-      this.products = response.data;
-    }, error => {
-      console.log(error);
-    });
   }
 }

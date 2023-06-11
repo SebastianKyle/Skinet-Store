2de3554ff6a49b89045540f8410aa97e1d2917fc
SebastianKyle <doantanqn000@gmail.com>
2023-06-12 00:50:51 +0700

    ApiUrl updated

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
   apiUrl: 'https://localhost:7007/api/'
+  // apiUrl: 'https://dmt-skinet-store.azurewebsites.net/api/'
 };
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://dmt-skinet-store.azurewebsites.net/api/'
+  apiUrl: 'https://dmt-skinet.azurewebsites.net/api/'
 };
